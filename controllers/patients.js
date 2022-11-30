b81f1837056fc20e628b21750d1e1ff8117b1f96
RodrigoRomanDev <rodrigo.roman.developer@gmail.com>
2022-11-29 18:55:06 -0600

    new pdf format

--- controllers/patients.js
+++ controllers/patients.js
@@ -404,7 +404,7 @@ module.exports.accountToPDF = async (req,res) =>{
     // await page.goto(`https://warm-forest-49475.herokuapp.com/patients/${req.params.id}/showAccount?begin=${begin}&end=${end}`,{
     //     waitUntil: 'networkidle0'});          // go to site
     await page.goto(
-        `http://localhost:3000/patients/${req.params.id}/showAccount?begin=${begin}&end=${end}`,{
+        `https://dinoclinic-production.up.railway.app/patients/${req.params.id}/showAccount?begin=${begin}&end=${end}`,{
           waitUntil: 'networkidle0'});
 
     const dom = await page.$eval('.toPDF', (element) => {
@@ -454,11 +454,11 @@ module.exports.dischAccountPDF = async (req,res) =>{
     
     // await page.goto(`https://pure-brushlands-42473.herokuapp.com/patients/${req.params.id}/showAccount?begin=${begin}&end=${end}`,{
     //     waitUntil: 'networkidle0'}); 
-    // await page.goto(`https://dinoclinic-production.up.railway.app/patients/${req.params.id}/showDischarged`,{
-    //     waitUntil: 'networkidle0'});          // go to site
-    await page.goto(
-        `http://localhost:3000/patients/${req.params.id}/showDischarged`,{
-          waitUntil: 'networkidle0'});
+    await page.goto(`https://dinoclinic-production.up.railway.app/patients/${req.params.id}/showDischarged`,{
+        waitUntil: 'networkidle0'});          // go to site
+    // await page.goto(
+    //     `http://localhost:3000/patients/${req.params.id}/showDischarged`,{
+    //       waitUntil: 'networkidle0'});
 
     const dom = await page.$eval('.toPDF', (element) => {
         return element.innerHTML
